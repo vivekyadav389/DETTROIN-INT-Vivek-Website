cad75dd16d9f850544b2e475a6a18249aeb1543f
vivekyadav389 <vivekya9504@gmail.com>
2026-07-25 17:47:24 +0530

    Refactor HomePage to use named component exports

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,10 +5,10 @@ import { KisStatsBar } from '../components/StatsBar';
 import { KisAbout } from '../components/AboutSection';
 import { KisFeatures } from '../components/Features';
 import { KisPrincipal } from '../components/PrincipalMessage';
-import EventsPreview from '../components/EventsPreview';
-import GalleryGrid from '../components/GalleryGrid';
-import CTABanner from '../components/CTABanner';
-import Footer from '../components/Footer';
+import { EventsPreview } from '../components/EventsPreview';
+import { GalleryGrid } from '../components/GalleryGrid';
+import { CTABanner } from '../components/CTABanner';
+import { Footer } from '../components/Footer';
 
 const HomePage = () => {
   return (
